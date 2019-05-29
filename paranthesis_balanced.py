3f9694c5290977b90e4d782c35a1b711545adb0a
AlexanderNSU <48973015+AlexanderNSU@users.noreply.github.com>
2019-05-29 20:02:29 +0700

    Brackets

--- paranthesis_balanced.py
+++ paranthesis_balanced.py
@@ -2,5 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def is_paranthesis_balanced(s):
-    pass
-
+    dct={'(':')','[':']','{':'}','<':'>'}
+    if len(s)==0:
+        return True
+    stack = Stack(len(s))
+    for i in range(len(s)):
+        if s[i] in ['(', '[', '{', '<']:
+            stack.push(s[i])
+        else:
+            if dct.get(stack.pop())==s[i]:
+                continue
+            else:
+                return False
+    if stack.__len__() == 0:
+        return True
